41caea65bae1e2f47583e986ebb5af4beba698bd
daishaoyang <dai1184749173@163.com>
2020-04-06 19:26:41 +0800

    代少杨第四次提交

---
+++ DsyManager.h
@@ -0,0 +1,18 @@
+//
+//  DsyManager.h
+//  DsyBase
+//
+//  Created by daishaoyang on 2020/4/6.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DsyManager : NSObject
+
+- (void)print;
+
+@end
+
+NS_ASSUME_NONNULL_END
